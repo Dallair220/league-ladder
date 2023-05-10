d2fc0b6471ac7f1bf2aca36d837d4a8f57181c0c
Dallair220 <93786532+Dallair220@users.noreply.github.com>
2023-05-10 21:38:35 +0200

    Change key visibility

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-// development keys expire in less than 24 hours.
-const DEV_RIOT_KEY = 'RGAPI-31364e5d-25fc-48e0-94ed-46e2f9b82340';
+import getRiotAPIKey from './key';
+
+// development keys expire within 24 hours.
+const DEV_RIOT_KEY = getRiotAPIKey();
