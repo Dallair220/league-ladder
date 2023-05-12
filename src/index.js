a1c88682150e4adb61113a4226946318d62d8c4c
Dallair220 <93786532+Dallair220@users.noreply.github.com>
2023-05-12 11:56:18 +0200

    Fixed not finding main.js issue

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import sortAllRanks from './ladder';
 
 console.log('running.');
 
-// sortAllRanks([
-//   'Silhim',
-//   'LVance',
-//   'Scintmar',
-//   'Schmongo',
-//   'Dimantus',
-//   'Fertig11',
-//   'Hornycopter',
-//   'GeneralMuh',
-//   'M4pl3g0d',
-//   'GUCCISCRIPTER',
-//   'Keep0ut',
-//   'AdmiralAlpaka',
-//   'KrokoEfendi',
-//   'Escapes',
-//   'LumpiDumpiDu',
-//   'Carthour',
-// ]);
+sortAllRanks([
+  'Silhim',
+  'LVance',
+  'Scintmar',
+  'Schmongo',
+  'Dimantus',
+  'Fertig11',
+  'Hornycopter',
+  'GeneralMuh',
+  'M4pl3g0d',
+  'GUCCISCRIPTER',
+  'Keep0ut',
+  'AdmiralAlpaka',
+  'KrokoEfendi',
+  'Escapes',
+  'LumpiDumpiDu',
+  'Carthour',
+]);
